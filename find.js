16bfff9de1881a8ef4a2bd0d307ac4d6b4740774
ykadir42 <ykadir42@gmail.com>
2017-12-13 13:25:10 -0500

    Added color and randomness

--- find.js
+++ find.js
@@ -4,9 +4,9 @@ var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
-
+var targetX = Math.random() * boxWidth;
+var targetY = Math.random() * boxHeight;
+var counter = 0;
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
@@ -20,23 +20,36 @@ var distance = function (x0, y0, x1, y1) {
 var findIt = function(e) {
     //console.log(e.x);
     //console.log(e.y);
-	//console.log(distance(e.x, e.y, targetX, targetY));
-	var color = getColor(distance(e.x, e.y, targetX, targetY));
-	console.log(color);
-	box.style.backgroundColor = color;
+    //console.log(distance(e.x, e.y, targetX, targetY));
+    var color = getColor(distance(e.x, e.y, targetX, targetY));
+    console.log(color);
+    box.style.backgroundColor = color;
 };
 
 var getColor = function(dist){
-	var blackDist = distance(0, 0, boxHeight, boxWidth) / 2;
-	//console.log((Math.floor(256 - (256 * (dist / blackDist)))).toString(16));
-	var color = Math.floor(256 - (256 * (dist / blackDist)))
-	if(color < 0) color = 0;
-	if(color > 255) color = 255;
-	if(color > 15) hex = color.toString(16);
-	else hex = "0" + color.toString(16);
-	//console.log("#" + hex + hex + hex);
-	return "#" + hex + hex + hex;
+    var blackDist = distance(0, 0, boxHeight, boxWidth);
+    //console.log((Math.floor(256 - (256 * (dist / blackDist)))).toString(16));
+    var color = Math.floor(100 - (100 * (dist / blackDist)))
+    if(color < 0) color = 0;
+    if(color > 100) color = 100;
+    color = "hsl(216, 100%, " + color + "%)";
+    return color;
+}
+
+var isFound = function(e){
+    console.log(distance(e.x, e.y, targetX, targetY));
+    var epsilon = 50;
+    if(counter == 0 && distance(e.x, e.y, targetX, targetY) < epsilon){
+	console.log("Good job! You found it!");
+	box.style.backgroundColor = "red";
+	var msg = document.createElement("h1");
+	msg.innerHTML="Good job! You found it!";
+	msg.setAttribute("style", "text-align:center;");
+	box.appendChild(msg);
+	console.log(msg);
+	counter++;
+    }
 }
 
 box.addEventListener("mousemove", findIt);
-
+box.addEventListener("click", isFound);
