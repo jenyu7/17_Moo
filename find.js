d47d34abce05cfcaef0de8338e2285d84159b9e2
ykadir42 <ykadir42@gmail.com>
2017-12-13 00:38:07 -0500

    Finished grayscale

--- find.js
+++ find.js
@@ -18,13 +18,24 @@ var distance = function (x0, y0, x1, y1) {
 
 
 var findIt = function(e) {
-    
+    //console.log(e.x);
+    //console.log(e.y);
+	//console.log(distance(e.x, e.y, targetX, targetY));
+	var color = getColor(distance(e.x, e.y, targetX, targetY));
+	console.log(color);
+	box.style.backgroundColor = color;
 };
 
-/*
-your OTHER FXNS
-
-*/
+var getColor = function(dist){
+	var blackDist = distance(0, 0, boxHeight, boxWidth) / 2;
+	//console.log((Math.floor(256 - (256 * (dist / blackDist)))).toString(16));
+	var color = Math.floor(256 - (256 * (dist / blackDist)))
+	if(color < 0) color = 0;
+	if(color > 255) color = 255;
+	hex = color.toString(16);
+	//console.log("#" + hex + hex + hex);
+	return "#" + hex + hex + hex;
+}
 
 box.addEventListener("mousemove", findIt);
 
